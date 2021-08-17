7d96f1729309b04148fccb49b16c5f518382f8fa
amirasadi <a.m.asadi2001@gmail.com>
2021-08-17 19:12:41 +0430

    almost finished posts in main page

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -13,7 +13,7 @@ export default function ContactForm() {
               type="text"
               autoComplete="name"
               required
-              className="block w-full bg-transparent outline-none border border-opacity-25 focus:border-opacity-100 p-1 rounded-lg"
+              className="block w-full bg-transparent outline-none border border-opacity-25 focus:border-opacity-100 p-1 rounded-md"
             />
             <label htmlFor="name">ایمیل</label>
             <input
@@ -22,7 +22,7 @@ export default function ContactForm() {
               type="email"
               autoComplete="email"
               required
-              className="block w-full bg-transparent outline-none border border-opacity-25 focus:border-opacity-100 p-1 rounded-lg"
+              className="block w-full bg-transparent outline-none border border-opacity-25 focus:border-opacity-100 p-1 rounded-md"
             />
             <textarea
               id="message"
@@ -30,7 +30,7 @@ export default function ContactForm() {
               placeholder="پیام"
               required
               rows="3"
-              className="w-full my-8 bg-transparent outline-none border border-opacity-25 focus:border-opacity-100  px-5 py-2 rounded-lg"
+              className="w-full my-8 bg-transparent outline-none border border-opacity-25 focus:border-opacity-100  px-5 py-2 rounded-md"
             />
             <button
               type="submit"
--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,23 @@ import MainSlider from "../components/MainSlider";
 
 export default function Home() {
   return (
-    <section className="flex justify-end">
-      
+    <section className="flex justify-end  container mx-auto">
       <Head>
         <title>bloge ziba</title>
         <link rel="icon" href="/bz-logo.png" />
       </Head>
 
       <div className="w-full flex flex-col lg:flex-row h-[5000px] lg:h-[100vh] items-center justify-center">
-        <MainSlider />
-        <MainBlog />
-      </div>
 
+        <div className="lg:fixed lg:right-24">
+          <MainSlider/>
+        </div>
+
+        <div className="w-[50%] hidden lg:block"></div>
+
+        <MainBlog />
+
+      </div>
     </section>
   );
 }
